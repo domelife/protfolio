b65fc5c3f39aef690330e9f5a9b4213b5d0584ea
domelife <118324430+domelife@users.noreply.github.com>
2022-12-20 16:54:36 +0900

    fsg

--- example/vogue/js/js.js
+++ example/vogue/js/js.js
@@ -11,6 +11,10 @@ $('#gnb li:nth-child(3)').click(function() {
 
     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
     $('section>div:nth-child(3)').addClass('on');
+    $('nav').removeClass('on')
+    $('section').removeClass('on')
+    $('.btnMenu').fadeIn()
+
 
 })
 
@@ -22,6 +26,9 @@ $('#gnb li:nth-child(1)').click(function() {
 
     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
     $('section>div:nth-child(1)').addClass('on');
+    $('nav').removeClass('on')
+    $('section').removeClass('on')
+    $('.btnMenu').fadeIn()
 
 })
 
@@ -33,8 +40,32 @@ $('#gnb li:nth-child(2)').click(function() {
 
     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
     $('section>div:nth-child(2)').addClass('on');
+    $('nav').removeClass('on')
+    $('section').removeClass('on')
+    $('.btnMenu').fadeIn()
 
 })
+
+
+
+
+
+
+
+//btn을 클릭했을 때 nav가 왼쪽 기준으로 오른쪽이 열리고 
+//section은 오른쪽을 기준으로 왼쪽이 열린다.
+
+$('.btnMenu').click(function(){
+    // $('nav').css({'transform':'rotateY(15deg)'});
+    // $('section').css({'transform':'rotateY(-10deg)'});
+    
+    $('nav').addClass('on')
+    $('section').addClass('on')
+    $(this).fadeOut()
+
+
+})
+
 
 
 
