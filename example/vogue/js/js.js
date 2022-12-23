c1fa049b419a436dcececf646a7b77ba2d1adf2a
domelife <118324430+domelife@users.noreply.github.com>
2022-12-23 16:52:59 +0900

    adgdfgs

--- example/vogue/js/js.js
+++ example/vogue/js/js.js
@@ -4,42 +4,57 @@ $(document).ready(function() {
 
 // addEventlistener의 경우 이벤트롸 함수의 조합으로 표현한다.
 
-$('#gnb li:nth-child(3)').click(function() {
+// $('#gnb li:nth-child(3)').click(function() {
 
-    //모든 section의 div의 클래스명의 on값을 지워라.
-    $('section>div').removeClass('on');
+//     //모든 section의 div의 클래스명의 on값을 지워라.
+//     $('section>div').removeClass('on');
 
-    // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
-    $('section>div:nth-child(3)').addClass('on');
-    $('nav').removeClass('on')
-    $('section').removeClass('on')
-    $('.btnMenu').fadeIn()
-
-
-})
+//     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
+//     $('section>div:nth-child(3)').addClass('on');
+//     $('nav').removeClass('on')
+//     $('section').removeClass('on')
+//     $('.btnMenu').fadeIn()
 
 
-$('#gnb li:nth-child(1)').click(function() {
-
-    //모든 section의 div의 클래스명의 on값을 지워라.
-    $('section>div').removeClass('on');
-
-    // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
-    $('section>div:nth-child(1)').addClass('on');
-    $('nav').removeClass('on')
-    $('section').removeClass('on')
-    $('.btnMenu').fadeIn()
-
-})
+// })
 
 
-$('#gnb li:nth-child(2)').click(function() {
+// $('#gnb li:nth-child(1)').click(function() {
 
-    //모든 section의 div의 클래스명의 on값을 지워라.
-    $('section>div').removeClass('on');
+//     //모든 section의 div의 클래스명의 on값을 지워라.
+//     $('section>div').removeClass('on');
 
-    // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
-    $('section>div:nth-child(2)').addClass('on');
+//     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
+//     $('section>div:nth-child(1)').addClass('on');
+//     $('nav').removeClass('on')
+//     $('section').removeClass('on')
+//     $('.btnMenu').fadeIn()
+
+// })
+
+
+// $('#gnb li:nth-child(2)').click(function() {
+
+//     //모든 section의 div의 클래스명의 on값을 지워라.
+//     $('section>div').removeClass('on');
+
+//     // section의 div 중 3번째 아이에게 클래스명에 on값을 더해주어라.
+//     $('section>div:nth-child(2)').addClass('on');
+//     $('nav').removeClass('on')
+//     $('section').removeClass('on')
+//     $('.btnMenu').fadeIn()
+
+// })
+
+// 위의 함수 축약하기
+
+$('#gnb li').click(function(){
+
+    let i = $(this).index()
+
+    $('section>div').removeClass('on')
+    $('section>div').eq(i).addClass('on')
+
     $('nav').removeClass('on')
     $('section').removeClass('on')
     $('.btnMenu').fadeIn()
