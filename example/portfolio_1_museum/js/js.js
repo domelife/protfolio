e079198ee1fd44c998189f92d60cccaf7322db1d
domelife <118324430+domelife@users.noreply.github.com>
2023-01-18 16:45:41 +0900

    asdfasdf

--- example/portfolio_1_museum/js/js.js
+++ example/portfolio_1_museum/js/js.js
@@ -12,18 +12,32 @@ $(document).ready(function(){
     console.log(gnbwd)
 
 
-    //문제점: 화면크기가 조정될때마다 달라짐
-    $('section').height(ht)
+    // 문제점: 화면크기가 조정될때마다 달라짐
+    $('.main').height(ht);
+    $('.category').height(ht);
+    $('.info').height(ht);
+    $('.reservation').height(ht);
+    $('.floor').height(ht);
+    $('.notice').height(ht);
+
+    $('.road').height('1971px');
 
 
 
 
-    //해결책: 브라우저가 리사이즈 될때마다, 브라우저의 높이를 찾아 
-    //section의 높이값에 대입해라
+    // 해결책: 브라우저가 리사이즈 될때마다, 브라우저의 높이를 찾아 
+    // section의 높이값에 대입해라
     $(window).resize(function(){
 
         let ht = $(window).height();
-        $('section').height(ht)
+        $('.main').height(ht);
+        $('.category').height(ht);
+        $('.info').height(ht);
+        $('.reservation').height(ht);
+        $('.floor').height(ht);
+        $('.notice').height(ht);
+
+        $('.road').height('1971px');
     })
 
 
